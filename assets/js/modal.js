c4c475bbab2290c4d0f289f2a3cf96ab11b9597e
ajayieee <ajaypgdi123@gmail.com>
2026-09-16 10:15:05 +0530

    modal-1

---
+++ assets/js/modal.js
@@ -0,0 +1,60 @@
+
+const vehicleModal = document.getElementById("vehicleModal");
+const vehicleModalClose = document.getElementById("vehicleModalClose");
+
+// All Add a vehicle buttons
+const vehicleButtons = document.querySelectorAll(".vehicleCard_select");
+
+
+// Open popup
+vehicleButtons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+        vehicleModal.classList.add("active");
+
+        // Prevent background scrolling
+        document.body.style.overflow = "hidden";
+
+    });
+
+});
+
+
+// Close popup
+vehicleModalClose.addEventListener("click", () => {
+
+    vehicleModal.classList.remove("active");
+
+    document.body.style.overflow = "";
+
+});
+
+
+// Close when clicking outside modal
+vehicleModal.addEventListener("click", (event) => {
+
+    if (event.target === vehicleModal ||
+        event.target.classList.contains("vehicleModal_overlay")) {
+
+        vehicleModal.classList.remove("active");
+
+        document.body.style.overflow = "";
+
+    }
+
+});
+
+
+// Close with ESC key
+document.addEventListener("keydown", (event) => {
+
+    if (event.key === "Escape") {
+
+        vehicleModal.classList.remove("active");
+
+        document.body.style.overflow = "";
+
+    }
+
+});
